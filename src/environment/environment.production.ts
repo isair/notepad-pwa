56916b36f3b14d542044b3a498e9672ef0853d41
Baris Sencan <baris.sncn@gmail.com>
2020-06-10 14:59:47 +0100

    feat(app): Expand accepted types

--- src/environment/environment.production.ts
+++ src/environment/environment.production.ts
@@ -2,9 +2,38 @@ export const productionEnvironment = {
   defaultTabTitle: 'New File',
   accepts: [
     {
-      description: 'Text file',
+      description: 'Text',
       extensions: ['txt'],
-      mimeTypes: ['text/plain'],
     },
-  ],
+    {
+      description: 'Markdown',
+      extensions: ['md'],
+    },
+    {
+      description: 'Data',
+      extensions: ['json', 'yml', 'xml', 'csv'],
+    },
+    {
+      description: 'Code',
+      extensions: [
+        'vue',
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'c',
+        'cpp',
+        'h',
+        'm',
+        'mm',
+        'swift',
+        'java',
+        'html',
+        'css',
+        'scss',
+        'styl',
+        'sass',
+      ],
+    },
+  ].map(accept => ({ ...accept, mimeTypes: ['text/plain'] })),
 };
