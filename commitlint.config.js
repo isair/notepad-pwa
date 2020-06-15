16773ed2392cc32a7a81da001c9f8cb0b87d9c35
Baris Sencan <baris.sncn@gmail.com>
2020-06-15 18:24:15 +0100

    chore(app): Clean up

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const scopes = require('./commitlint.scopes');
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
-    'scope-enum': [2, 'always', scopes],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'multi',
+        'config',
+        'github',
+        'script',
+        'deps',
+        'dev-deps',
+        'app',
+        'release',
+      ],
+    ],
     'subject-case': [2, 'always', ['sentence-case']],
   },
 };
